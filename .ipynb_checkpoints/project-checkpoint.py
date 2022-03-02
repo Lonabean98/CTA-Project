06c0db428ca345579b17fe5ed16ed8a1b8813cb8
Lonan Keane <lonankeane98@gmail.com>
2022-03-02 18:39:14 +0100

    adding bubble sort algorithm

---
+++ .ipynb_checkpoints/project-checkpoint.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+rng = np.random.default_rng(12345)
+rints = rng.integers(low=0, high=10, size=3)
+rints
--- project.py
+++ project.py
@@ -1,4 +1,19 @@
 import numpy as np
 
+
 rng = np.random.default_rng()
-rng.integers(2, size=10)
+list= rng.integers(low=0, high=10, size=50)
+
+
+# From: https://stackabuse.com/bubble-sort-in-python/
+def bubble_sort(our_list):
+    # Go through the array as many times as there are elements in the array
+    for i in range(len(our_list)):
+        # We want the last pair of adjacent elements to be (n-2, n-1)
+        for j in range(len(our_list) - 1):
+            if our_list[j] > our_list[j+1]:
+                # Swap
+                our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
+
+bubble_sort(list)
+print(list)
